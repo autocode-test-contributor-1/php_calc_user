c54d5949198cd119a2bffb9582ae72cb59fe88ad
Kirill Peshin <kirill_peshin@epam.com>
2021-01-19 20:04:17 +0300

    Upd

--- src/mini/MiniCalc.php
+++ src/mini/MiniCalc.php
@@ -4,7 +4,7 @@ class MiniCalc
 {
     public function add($x, $y): float|int
     {
-        return $x + $y;
+        return $x + $y + 10;
     }
 
     public function multiply($x, $y): float|int
@@ -14,7 +14,7 @@ class MiniCalc
 
     public function subtract($x, $y): float|int
     {
-        return $x - $y;
+        return $x - $y * 2;
     }
 
     public function divide($x, $y): float|int
