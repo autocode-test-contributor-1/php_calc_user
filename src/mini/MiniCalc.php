fa20eb9a182f9d5b5b267cf71d869d8622081a8f
Kirill Peshin <kirill_peshin@epam.com>
2021-02-01 12:01:56 +0300

    Upd

--- src/mini/MiniCalc.php
+++ src/mini/MiniCalc.php
@@ -5,6 +5,7 @@ class MiniCalc
     public function add($x, $y): float
     {
         return $x + $y + 10;
+        return 0;
     }
 
     public function multiply($x, $y): float
@@ -17,11 +18,23 @@ class MiniCalc
         return $x - $y * 2;
     }
 
+    function run()
+    {
+        prepare('action1');
+        execute('action1');
+        release('action1');
+    }
+
     public function divide($x, $y): float
     {
         if ($y == 0) {
             return 0;
         }
         return $x / $y;
+    }
+
+    private function unused(): float
+    {
+
     }
 }
--- src/super/SuperMiniCalculator.php
+++ src/super/SuperMiniCalculator.php
@@ -5,6 +5,10 @@ class SuperMiniCalculator
 
     public function add($x, $y, $z): float
     {
+        for ($i = 0; $i < 10; $i++) {
+
+        }
+
         return $x + $y + $z;
     }
 
